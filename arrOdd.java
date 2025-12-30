83e81e3c0afe4fa1179fe0a3a9fa16475a1652a5
prachipratyushaacharya <prachipratyushaacharya@gmail.com>
2025-12-30 12:21:34 +0530

    arrays

---
+++ arrOdd.java
@@ -0,0 +1,34 @@
+package Arrays;
+
+import java.util.Scanner;
+
+public class arrOdd {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] a = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            a[i] = sc.nextInt();
+        }
+
+        // // Print odd numbers
+        // for (int i = 1;i<n; i+=2){
+        // System.out.println(a[i] + " ");
+        // }
+
+        // elment is the sum of perfect suqares
+
+        int sum = 0;
+
+        for (int i = 0; i < n; i++) {
+            int x = (int) Math.sqrt(a[i]);
+            if (x * x == a[i]) {
+                sum += a[i];
+            }
+        }
+
+        System.out.println("sum=" + sum);
+
+    }
+}
