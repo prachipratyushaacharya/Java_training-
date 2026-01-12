f6cb607fb3c87cfc31a0a115b3f8fd003269f46b
prachipratyushaacharya <prachipratyushaacharya@gmail.com>
2026-01-12 23:36:38 +0530

    functions

---
+++ function.java
@@ -0,0 +1,11 @@
+
+public class function {
+    public static void main(String[] args) {
+        fun("Thehouse");
+    }
+
+    public static void fun(String str) {
+        System.out.println("Hello " + str);
+    }
+
+}
