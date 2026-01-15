f3f6d974c67ca26713007ef0aa71dcf6a8f70754
prachipratyushaacharya <prachipratyushaacharya@gmail.com>
2026-01-15 12:35:55 +0530

    bubble sort

---
+++ bubbleSort.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class bubbleSort {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] a = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            a[i] = sc.nextInt();
+        }
+
+        // bubble sort logic
+
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (a[j] > a[j + 1]) {
+                    swap(a, j, j + 1);
+                }
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            System.out.println(a[i] + " ");
+        }
+    }
+
+    public static void swap(int[] arr, int x, int y) {
+        int temp = arr[x];
+        arr[x] = arr[y];
+        arr[y] = temp;
+    }
+}
